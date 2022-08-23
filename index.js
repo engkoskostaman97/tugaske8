f9617514bc4baac0886da6e835af6974d6b2a619
penerus programmer <engkoskostaman759@gmail.com>
2022-08-23 17:30:18 +0700

    perubahan mokup card

--- index.js
+++ index.js
@@ -12,23 +12,21 @@ let isLogin = true
 let dataBlog = []
 
 app.get('/', function (request, response) {
-    // console.log(dataBlog);
+    console.log(dataBlog);
 
     let data = dataBlog.map(function (item) {
         return {
             ...item,
-            isLogin,
-            postAt: getFullTime(item.postAt),
-            duration: getDistanceTime(item.postAt)
+            // date: getFullTime(item.duration),
+            duration: getDistanceTime(new Date(item.startDate), new Date(item.endDate)),
+
         }
     })
 
-    response.render('index', { isLogin, blog: data })
+    response.render('index', { blog: data })
 })
 
-app.get('/contact', function (request, response) {
-    response.render('contact')
-})
+
 
 
 
@@ -38,37 +36,46 @@ app.get('/blog-detail/:index', function (request, response) {
     let data = dataBlog[index]
     data = {
         title: data.title,
-        content: data.content,
-        stardate: data.stardate,
-        enddate: data.enddate,
         author: data.author,
-        postAt: getFullTime(data.postAt)
+        duration: getDistanceTime(new Date(data.startDate), new Date(data.endDate)),
+        startDate: data.startDate,
+        endDate: data.endDate,
+        content: data.content,
+
+
     }
 
     response.render('blog-detail', { data })
 })
 
+app.get('/contact', function (request, response) {
+    response.render('contact')
+})
 app.get("/myproject", function (request, response) {
     response.render("myproject");
 });
 
 
 app.post('/myproject', function (request, response) {
+
     // console.log(request.body);
+    let title = request.body.inputTitle;
+    let startDate = request.body.inputStartDate;
+    let endDate = request.body.inputEndDate;
+    let content = request.body.inputContent;
 
-    let title = request.body.inputTitle
-    let content = request.body.inputContent
-    let stardate = request.body.inputStardate
-    let enddate = request.body.inputEnddate
 
-
+    console.log(title);
+    console.log(startDate);
+    console.log(endDate);
+    console.log(content);
 
     let blog = {
         title,
-        stardate,
-        enddate,
+        duration: new Date(),
+        startDate,
+        endDate,
         content,
-        postAt: new Date(),
         author: "Engkos Kostaman",
     }
 
@@ -76,18 +83,13 @@ app.post('/myproject', function (request, response) {
 
     response.redirect('/')
 })
-
+app.get("/update-blog", function (request, response) {
+    response.render("update-blog");
+});
 app.get('/update-blog/:index', function (request, response) {
     let index = request.params.index
 
-    let data = {
-        title: dataBlog[index].title,
-        content: dataBlog[index].content,
-        stardate: dataBlog[index].stardate,
-        enddate: dataBlog[index].enddate
-    }
-
-    response.render('update-blog', { index, data })
+    response.render('update-blog', { index })
 })
 
 app.post('/update-blog/:index', function (request, response) {
@@ -96,8 +98,8 @@ app.post('/update-blog/:index', function (request, response) {
 
     dataBlog[index].title = request.body.inputTitle
     dataBlog[index].content = request.body.inputContent
-    dataBlog[index].stardate = request.body.inputStardate
-    dataBlog[index].enddate = request.body.inputEnddate
+    dataBlog[index].startDate = request.body.inputStartDate
+    dataBlog[index].endDate = request.body.inputEndDate
 
     response.redirect('/')
 })
@@ -113,55 +115,55 @@ app.get('/delete-blog/:index', function (request, response) {
 
 
 function getFullTime(time) {
+    let month = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 
-    let month = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
+    let date = time.getDate();
+    let monthIndex = time.getMonth();
+    let year = time.getFullYear();
 
-    let date = time.getDate()
-    let monthIndex = time.getMonth()
-    let year = time.getFullYear()
-
-    let hours = time.getHours()
-    let minutes = time.getMinutes()
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
 
     if (hours < 10) {
-        hours = "0" + hours
+        hours = "0" + hours;
     } else if (minutes < 10) {
-        minutes = "0" + minutes
+        minutes = "0" + minutes;
     }
 
-    // 12 Agustus 2022 09.04
-    let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`
-    // console.log(fullTime);
-    return fullTime
+    let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+    return fullTime;
 }
 
-function getDistanceTime(time) {
+function getDistanceTime(time, end) {
+    let timeNow = end;
+    let timePost = time;
 
-    let timeNow = new Date()
-    let timePost = time
+    let distance = timeNow - timePost;
 
-    let distance = timeNow - timePost
-    // console.log(distance);
+    let milisecond = 1000;
+    let secondInHours = 3600;
+    let hoursInDay = 24;
+    let daysInMonth = 30;
 
-    let milisecond = 1000 // 1 detik 1000 milisecond
-    let secondInHours = 3600 // 1 jam sama dengan 3600 detik
-    let hoursInDay = 24 // 1 hari 24 jam
+    let distanceMonth = Math.floor(distance / (milisecond * secondInHours * hoursInDay * daysInMonth));
+    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay));
+    let distanceHours = Math.floor(distance / (milisecond * 60 * 60));
+    let distanceMinutes = Math.floor(distance / (milisecond * 60));
+    let distanceSeconds = Math.floor(distance / milisecond);
 
-    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
-    let distanceHours = Math.floor(distance / (milisecond * 60 * 60))
-    let distanceMinutes = Math.floor(distance / (milisecond * 60))
-    let distanceSeconds = Math.floor(distance / milisecond)
-
-    if (distanceDay > 0) {
-        return `${distanceDay} day ago`
+    if (distanceMonth > 0) {
+        return `${distanceMonth} months `;
+    } else if (distanceDay > 0) {
+        return `${distanceDay} days `;
     } else if (distanceHours > 0) {
-        return `${distanceHours} hours ago`
+        return `${distanceHours} hours `;
     } else if (distanceMinutes > 0) {
-        return `${distanceMinutes} minutes ago`
+        return `${distanceMinutes} minutes`;
     } else {
-        return `${distanceSeconds} seconds ago`
+        return `${distanceSeconds} seconds`;
     }
 }
+
 
 app.listen(port, function () {
     console.log(`server running on port ${port}`);
